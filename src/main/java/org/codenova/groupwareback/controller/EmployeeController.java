85c6bb19f75545d06a9f95a5a9aff12b4317853f
dap <ozalice011202@gmail.com>
2025-04-29 17:54:20 +0900

    feat : API추가 (전체사원정보, 사원검증)

--- src/main/java/org/codenova/groupwareback/controller/EmployeeController.java
+++ src/main/java/org/codenova/groupwareback/controller/EmployeeController.java
@@ -10,12 +10,14 @@ import org.codenova.groupwareback.repository.DepartmentRepository;
 import org.codenova.groupwareback.repository.EmployeeRepository;
 import org.codenova.groupwareback.repository.SerialRepository;
 import org.codenova.groupwareback.request.AddEmployee;
+import org.codenova.groupwareback.request.Login;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -28,15 +30,21 @@ public class EmployeeController {
     private final DepartmentRepository departmentRepository;
     private final SerialRepository serialRepository;
 
+    @GetMapping
+    public ResponseEntity<List<Employee>> getEmployeeHandle() {
+        List<Employee> list =employeeRepository.findAll();
+        return ResponseEntity.status(200).body(list);
+    }
+
     @PostMapping
     @Transactional
-    public ResponseEntity<Employee> postEmployeeHandle(@ModelAttribute @Valid AddEmployee addEmployee,
+    public ResponseEntity<Employee> postEmployeeHandle(@RequestBody @Valid AddEmployee addEmployee,
                                                        BindingResult result) {
         if(result.hasErrors()) {
             return ResponseEntity.status(400).body(null);  // bad request : 서버가 클라이언트 오류를 감지해 요청을 처리할 수 없는 코드
         }
         // 1. 사원번호 생성, 부서 객체
-        Optional<Serial> serial = serialRepository.findById(1); // JPA에서 id 로 찾는 걸 기본 제공해주는데, 결과가 Optional 객체가 나옴.
+        Optional<Serial> serial = serialRepository.findByRef("employee"); // JPA에서 id 로 찾는 걸 기본 제공해주는데, 결과가 Optional 객체가 나옴.
         // 정석적으로 사용한다면 serial.isPresent()  이런걸 확인해서 뽑아서 써야됨.
         Serial found = serial.get();    // 바로 뽑는 이유는 '100% 있는걸 알아서'
         Optional<Department> department = departmentRepository.findById(addEmployee.getDepartmentId());
@@ -62,15 +70,48 @@ public class EmployeeController {
         return ResponseEntity.status(201).body(employee);   // created : 요청이 성공적으로 처리되었으며, 자원이 생성되었음을 나타내는 성공 상태 응답 코드
     }
 
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyHandle(@RequestBody @Valid Login login, BindingResult result) {
+        if (result.hasErrors()) {
+            return ResponseEntity.status(400).body(null);
+        }
+        Optional<Employee> employee = employeeRepository.findById(login.getId());
+        if(employee.isEmpty() || !BCrypt.checkpw(login.getPassword(), employee.get().getPassword())) {
+            return ResponseEntity.status(401).body(null);
+        }
+
+        return ResponseEntity.status(200).body(employee.get());
+    }
+
 
 
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeDetailHandle(@PathVariable String id) {
+
         Optional<Employee> employee = employeeRepository.findById(id);
-        if(employee.isEmpty()) {
-            return ResponseEntity.status(404).body(null);
+        if (employee.isEmpty()) {
+            return ResponseEntity.status(404).body(null);   // not found : 서버가 요청받은 리소스를 찾을 수 없다는 것을 의미
         }
         return ResponseEntity.status(200).body(employee.get());
     }
 
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
